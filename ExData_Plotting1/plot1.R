3713087ad4fc819f1c5909c1827bc127bd4633a2
hanyuw <haan.cn@gmail.com>
2015-08-09 19:17:09 -0400

    initial commitment of plot1

---
+++ ExData_Plotting1/plot1.R
@@ -0,0 +1,17 @@
+## Hanyu Wang
+## Aug.9th
+## Coursera Exploratory Data Analysis Assignment 1
+## Plot 1
+
+# read the table
+HTCtable = read.table("household_power_consumption.txt", sep = ";", na.strings = "?", stringsAsFactors = FALSE, header = TRUE)
+
+# Select required time
+HTCselTable = subset(HTCtable, as.Date(HTCtable$Date, format = "%d/%m/%Y")>"2007-01-31"&as.Date(HTCtable$Date, format = "%d/%m/%Y")<"2007-02-03")
+
+# Plotting the graph
+hist(HTCselTable$Global_active_power, col = "RED", xlab = "Global Active Power (kilowatts)", main = "Global Active Power")
+
+## Saves plot to file
+dev.copy(png, file = "plot1.png", height = 480, width = 480)
+dev.off()
